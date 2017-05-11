6bf4cee3761f70a5efd9ce8a3e5ad8e668c9df74
Sergio Marin <serjey@gmail.com>
2017-05-11 19:15:35 +0200

    Añado las categorías al admin de django. El fix anterior para el "KeyError" no ha funcionado. He borrado todo y he ejecutado de nuevo las migraciones, tal como se indica en http://stackoverflow.com/questions/34548768/django-no-such-table-exception 1. Delete your database (db.sqlite3 in my case) in your project directory 2. Remove everything from __pycache__ folder under your project subdirectory 3. For the application you are trying to fix, go to the folder and clear migrations and __pycache__ directories
    When you are sure you have cleared all the above files, run:
    python manage.py makemigrations
    python manage.py migrate

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from blogs.models import Blog, Post
+from blogs.models import Blog, Post, Category
 
 admin.site.register(Blog)
 admin.site.register(Post)
+admin.site.register(Category)
