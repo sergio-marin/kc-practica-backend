067419a426d850c4886d0511b499992b27eb6c2b
Sergio Marin <serjey@gmail.com>
2017-05-11 18:47:08 +0200

    Añado las categorías al modelo. He tenido que eliminar los ficheros de anteriores migraciones porque obtenía un "KeyError" y realizar la migración como si fuera la primera vez

--- blogs/models.py
+++ blogs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Blog(models.Model):
-    blogger = models.OneToOneField(User, related_name="blogs")
+    blogger = models.OneToOneField(User, related_name="blog")
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=350, null=True, blank=True, default="")
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -13,12 +13,23 @@ class Blog(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
-    blog = models.ForeignKey(Blog, related_name="blogs")
+    blog = models.ForeignKey(Blog, related_name="posts")
     title = models.CharField(max_length=200)
     introduction = models.TextField(max_length=350)
     body = models.TextField()
     media_url = models.URLField(blank=True, null=True)
+    categories = models.ManyToManyField(Category, related_name="posts")
     published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
