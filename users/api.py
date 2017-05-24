8b7cb19414f91b86f8bee29aa9ae4fa1c923b8f3
Sergio Marin <serjey@gmail.com>
2017-05-24 22:12:08 +0200

    Implementado: Endpoint que permita ver el detalle de un usuario. Sólo podrán ver el endpoint de detalle de un usuario el propio usuario o un administrador.

--- users/api.py
+++ users/api.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import User
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 
+from users.permissions import UserPermission
 from users.serializers import UserSerializer
 
 
-class UserViewSet(CreateModelMixin, GenericViewSet):
+class UserViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
 
+    permission_classes = (UserPermission,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
---
+++ users/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework.permissions import BasePermission
+
+
+class UserPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        """
+        Define si un usuario puede usar o no un endpoint
+        :param request: HttpRequest
+        :param view: UserViewSet
+        :return: True si puede, False si no puede
+        """
+
+        # cualquiera puede crear un usuario
+        if view.action == "create":
+            return True
+
+        # cualquiera autenticado puede acceder al detalle para ver, actualizar o borrar
+        if request.user.is_authenticated() and view.action in "retrieve":
+            return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Define si un usuario puede realizar la acción sobre un objeto concreto
+        :param request: HttpRequest
+        :param view: UserViewSet
+        :param obj: User
+        :return: True si puede, False si no puede
+        """
+        # sólo si es admin o es el propio usuario
+        return request.user.is_superuser or request.user == obj
