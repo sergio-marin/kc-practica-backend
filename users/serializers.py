fc463c0e196067a2f34cc274a5c8415d30b99733
Sergio Marin <serjey@gmail.com>
2017-05-31 20:05:54 +0200

    Fix: al crear un usuario desde el endpoint la password se guardaba en texto plano

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,3 +9,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        return self.update(User(), validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        if validated_data.get('password'):
+            instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance
