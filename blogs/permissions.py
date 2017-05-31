6f8f87af3b0879a417145dc659bd9d4ae22b29a1
Sergio Marin <serjey@gmail.com>
2017-05-31 20:18:36 +0200

    Fix: limpiar el permission.py de blogs

--- blogs/permissions.py
+++ blogs/permissions.py
@@ -3,23 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class PostsPermissions(BasePermission):
 
-    def has_permission(self, request, view):
-        """
-        Definimos si un usuario puede usar o no el endpoint
-        :param request: HttpRequest
-        :param view: UserViewSet
-        :return: True si puede, False si no puede
-        """
 
-        # cualquiera puede listar o ver el detalle de un post publicado
-        if view.action in ("retrieve", "list"):
-            return True
-
-        # cualquiera autenticado puede crear, acceder al detalle, actualizar o borrar sus propios posts
-        if request.user.is_authenticated() and view.action in ("create", "update", "partial_update", "destroy"):
-            return True
-
-        return False
 
     def has_object_permission(self, request, view, obj):
         """
